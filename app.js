383e565682e7221b5333c00e743383b42597e905
UP2002235 <91495869+UP2002235@users.noreply.github.com>
2022-09-14 22:19:11 +0100

    Completed the main layout, keyboard function fully work, words being displayed and designed improved.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const tileContainer = document.querySelector(".tileContainer");
 const keyboardContainer = document.querySelector(".keyboardContainer");
 
+const wordOfTheDay = "SANJI"
+
 const keys = [
     'Q',
     'W',
@@ -32,14 +34,70 @@ const keys = [
     '«',
 ];
 
-const handleClick = () => {
-    console.log("Clicked");
+const tiles = [
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+];
+
+let currentRow = 0;
+let currentTile = 0;
+
+tiles.forEach((row, rowIndex) => {
+   const div = document.createElement("div");
+   div.setAttribute("id", "row-" + rowIndex);
+
+   row.forEach((tile, tileIndex) => {
+    const tileDiv = document.createElement("div");
+    tileDiv.setAttribute("id", "row-" + rowIndex + "-tile-" + tileIndex);
+    tileDiv.classList.add("tile");
+    div.append(tileDiv);
+   });
+
+   tileContainer.appendChild(div);
+});
+
+const handleClick = (key) => {
+    console.log("Clicked", key);
+    if (key === "«") {
+        deleteLetter();
+        return;
+    }
+    if (key === "ENTER") {
+        console.log("Check row");
+        return;
+    }
+    addLetter(key);
 };
 
 keys.forEach(key => {
     const button = document.createElement("button");
     button.textContent = key;
     button.setAttribute("id", key);
-    button.addEventListener("click", handleClick);
+    button.addEventListener("click", () => handleClick(key));
     keyboardContainer.append(button);
 });
+
+const addLetter = (letter) => {
+    if (currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById("row-" + currentRow + "-tile-" + currentTile);
+        tile.textContent = letter;
+        tiles[currentRow][currentTile] = letter;
+        tile.setAttribute("data", letter);
+        currentTile++;
+        console.log("tiles", tiles);
+    };
+};
+
+const deleteLetter = () => {
+    if (currentTile > 0) {
+        currentTile--;
+        const tile = document.getElementById("row-" + currentRow + "-tile-" + currentTile);
+        tile.textContent = "";
+        tiles[currentRow][currentTile] = "";
+        tile.setAttribute("data", "");
+    };
+};
